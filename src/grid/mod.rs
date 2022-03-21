ca6ba99c12eeae511d27f3901e5c0af969a1e209
immno <dancermno@gmail.com>
2022-03-21 17:42:21 +0800

    feat(grid): add grid

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -1,4 +1,30 @@
 use anyhow::Result;
 
+use crate::trade::TickerPrice;
+
+pub use self::fixed_grid_service::FixedGridService;
+
+mod fixed_grid_service;
+
 /// Abstraction of Grid services
-pub trait GridService {}
+pub trait GridService {
+    /// whether to buy
+    fn is_buy(&self, price: &TickerPrice) -> bool;
+
+    /// whether to sell
+    fn is_sell(&self, price: &TickerPrice) -> bool;
+
+    /// 调整数据
+    fn poise(&self) -> bool;
+    fn is_air(&self) -> bool;
+    fn modify_price(&self) -> bool;
+}
+
+#[derive(Default)]
+pub struct SimpleFactory;
+
+impl SimpleFactory {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,56 @@
+use std::ops::Deref;
+
 use anyhow::Result;
-use tracing::info;
+use tracing::{info, warn};
 
 pub use config::*;
 pub use error::TgError;
 
+use crate::grid::{FixedGridService, GridService};
+use crate::trade::{SimpleFactory, TradeFactory};
+
 mod config;
 mod error;
 mod grid;
-mod market;
 mod trade;
 
 /// 通过配置创建 KV 服务器
 #[tracing::instrument(skip_all)]
 pub async fn start_server_with_config(config: &ServerConfig) -> Result<()> {
-    info!("Starting Trend-Grid-Server");
-    info!("config:{:?}", config);
+    info!("Starting: Trend Grid Server");
+    let (market, trade) = SimpleFactory::new().make(config.trade.as_ref())?;
+
+    if let Some(eth) = config.coin.eth.as_ref() {
+        let curr_price = market.deref().ticker_price().await?;
+        let grid: FixedGridService = eth.into();
+        let grid: &dyn GridService = &grid;
+        loop {
+            if grid.is_buy(&curr_price) {
+                if let Ok(_) = trade.order() {
+                    grid.poise();
+                    info!("挂单成功")
+                } else {
+                    return continue;
+                }
+            } else if grid.is_sell(&curr_price) {
+                if grid.is_air() {
+                    grid.modify_price();
+                } else {
+                    if let Ok(_) = trade.order() {
+                        grid.poise();
+                    } else {
+                        return continue;
+                    }
+                }
+            } else {
+                warn!(
+                    "币种:{},当前市价：{}。未能满足交易,继续运行",
+                    "eth",
+                    curr_price.price.as_str()
+                )
+            }
+        }
+    };
 
     Ok(())
 }
