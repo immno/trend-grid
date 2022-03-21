10e238e842121b98f0edfce34cbeeec756e2155e
mno <shujiang.tao@cisdi.com.cn>
2022-03-21 22:27:27 +0800

    feat(bian api): learn reqwest

--- src/trade/binance_api_service.rs
+++ src/trade/binance_api_service.rs
@@ -10,8 +10,15 @@ pub struct BinanceTradeService {
     url: Url,
 }
 
+#[async_trait]
 impl TradeService for BinanceTradeService {
-    fn order(&self) -> anyhow::Result<bool> {
+    async fn buy_limit(&self, quantity: usize, price: Option<f64>) -> anyhow::Result<bool> {
+        let mut url = self.url.clone();
+        url.set_path("/fapi/v1/ping");
+        Ok(reqwest::get(url).await?.status().is_success())
+    }
+
+    async fn sell_limit(&self, quantity: usize, price: Option<f64>) -> anyhow::Result<bool> {
         todo!()
     }
 }
@@ -80,21 +87,73 @@ impl TryFrom<&TradeConfig> for BinanceMarketService {
 mod tests {
     use std::collections::HashMap;
 
+    use hex::ToHex;
+    use ring::hmac;
+
     use super::*;
 
     #[tokio::test]
     async fn ping() {
-        // let data = reqwest::get("https://fapi.binance.com/fapi/v1/time")
+        // let mut s = HashMap::new();
+        // s.insert("type", "MARKET");
+        // s.insert("symbol", "BTCUSDT");
+        // s.insert("side", "BUY");
+        // s.insert("quantity", "1");
+        // s.insert("recvWindow", "5000");
+        // let now = BinanceMarketService::get_now();
+        // s.insert("timestamp", now.as_str());
+        //
+        // let mut url = reqwest::Url::parse("https://fapi.binance.com/fapi/v1/order").unwrap();
+        // url.query_pairs_mut().clear();
+        //
+        // for (key, value) in s.iter() {
+        //     url.query_pairs_mut().append_pair(key, value);
+        // }
+        //
+        // let q = url.query().unwrap();
+        // let key = hmac::Key::new(
+        //     hmac::HMAC_SHA256,
+        //     "4b42ddee81f19826959a40249d339eaae87a2caac1bce690c18a5ca52a2c3cfd".as_bytes(),
+        // );
+        // let signature = hmac::sign(&key, q.as_bytes());
+        // let signature = hex::encode(signature.as_ref());
+        // s.insert("signature", signature.as_str());
+        // url.query_pairs_mut()
+        //     .append_pair("signature", signature.as_str());
+        //
+        // println!("{}", url.as_str());
+        // println!("https://fapi/binance.com/fapi/v1/order?symbol=BTCUSDT&side=BUY&type=LIMIT&quantity=1&price=9000&timeInForce=GTC&recvWindow=5000&timestamp=1591702613943&signature= 3c661234138461fcc7a7d8746c6558c9842d4e10870d2ecbedf7777cad694af9");
+        // let client = reqwest::Client::new();
+        // let mut build = client.post(url).header(
+        //     "X-MBX-APIKEY",
+        //     "5eb75348011c84276e69fd9f669a91fbd4a0e64e49405d0c218acc52ec600b8c",
+        // );
+        // let res = build
+        //     // .query(url.as_str())
+        //     .send()
         //     .await
         //     .unwrap()
-        //     .json::<HashMap<String, String>>()
+        //     .text()
         //     .await
         //     .unwrap();
-        // println!("{:?}", data);
+        // println!("{:?}", res);
     }
 
     #[test]
     fn sign() {
+        let key = hmac::Key::new(
+            hmac::HMAC_SHA256,
+            b"2b5eb11e18796d12d88f13dc27dbbd02c2cc51ff7059765ed9821957d82bb4d9",
+        );
+        let signature = hmac::sign(&key, b"symbol=BTCUSDT&side=BUY&type=LIMIT&quantity=1&price=9000&timeInForce=GTC&recvWindow=5000&timestamp=1591702613943");
+        assert_eq!(
+            "3c661234138461fcc7a7d8746c6558c9842d4e10870d2ecbedf7777cad694af9",
+            hex::encode(signature.as_ref())
+        );
+    }
+
+    #[test]
+    fn sign2() {
         let url = Url::parse("https://fapi.binance.com/fapi/v1/time?sd=23").unwrap();
         let mut url1 = url.clone();
         url1.set_path("/sdf/gfg");
