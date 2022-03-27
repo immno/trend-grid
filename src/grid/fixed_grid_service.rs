5f67c43a38ed3a17de6b7d0572842a5a7685b576
mno <shujiang.tao@cisdi.com.cn>
2022-03-27 23:08:00 +0800

    feat(grid): grid

--- src/grid/fixed_grid_service.rs
+++ src/grid/fixed_grid_service.rs
@@ -12,51 +12,57 @@ use crate::{Coin, Symbol, TgError};
 
 pub struct FixedGridService {
     symbol: Symbol,
-    pub market: Arc<dyn MarketService>,
-    pub trade: Arc<dyn TradeService>,
-    pub inner: Inner,
+    market: Arc<dyn MarketService>,
+    trade: Arc<dyn TradeService>,
+    db: Db,
 }
 
-pub struct Inner {
-    pub buy: f64,
-    pub sell: f64,
-    pub quantity: Vec<f64>,
-    pub profit_ratio: f64,
-    pub double_throw_ratio: f64,
-    pub step: usize,
-    pub history: Vec<f64>,
+pub struct Db {
+    buy: f64,
+    sell: f64,
+    quantity: f64,
+    profit_ratio: f64,
+    double_throw_ratio: f64,
+    history: Vec<f64>,
 }
 
 #[async_trait]
 impl GridService for FixedGridService {
     async fn execute(&mut self, price: f64) -> anyhow::Result<()> {
-        let symbol = self.symbol.borrow();
-        let quantity = self.buy_quantity();
-        if self.is_buy(price) {
-            if let Ok(Some(success_price)) = self.trade.buy(symbol, quantity).await {
-                self.reset_ratio().await?; // ok
-                self.record(success_price); // ok
-                self.modify_price(0, price, price);
-                info!("挂单成功");
-                tokio::time::sleep(Duration::from_secs(120)).await;
-            }
-        } else if self.is_sell(price) {
-            if self.is_air() {
-                // self.modify_price(price);
-            } else {
-                let sell_amount = self.get_quantity(false);
-                if let Ok(Some(success_price)) = self.trade.sell(symbol, 80.0).await {
-                    self.reset_ratio().await?; // ok
-
-                    // self.poise();
-                }
-            }
-        } else {
-            warn!(
-                "币种:{:?},当前市价：{}。未能满足交易,继续运行",
-                "symbol", price
-            );
-        }
+        // let symbol = self.symbol.borrow();
+        // let quantity = self.db.get_quantity();
+        // if self.is_buy(price) {
+        //     if let Ok(Some(success_price)) = self.trade.buy(symbol, quantity).await {
+        //         self.reset_ratio().await?;
+        //         self.add_record(success_price);
+        //         self.modify_price(0, price, price);
+        //         info!("挂单成功");
+        //         tokio::time::sleep(Duration::from_secs(120)).await;
+        //     }
+        // } else if self.is_sell(price) {
+        //     if self.is_air() {
+        //         self.modify_price(0, self.db.sell, price);
+        //     } else {
+        //         let last_price = self.db.last_record();
+        //         let sell_amount = self.db.get_quantity();
+        //         let porfit_usdt = price.sub(last_price).mul(sell_amount);
+        //
+        //         if let Ok(Some(success_price)) = self.trade.sell(symbol, sell_amount).await {
+        //             warn!(
+        //                 "报警：币种为：{}。卖单量为：{}。预计盈利{}U",
+        //                 symbol, sell_amount, porfit_usdt
+        //             );
+        //             self.reset_ratio().await?;
+        //             self.modify_price(-1, last_price, price);
+        //             self.remove_record();
+        //         }
+        //     }
+        // } else {
+        //     warn!(
+        //         "币种:{:?},当前市价：{}。未能满足交易,继续运行",
+        //         "symbol", price
+        //     );
+        // }
 
         Ok(())
     }
@@ -69,44 +75,32 @@ impl FixedGridService {
         market: Arc<dyn MarketService>,
         trade: Arc<dyn TradeService>,
     ) -> Result<Self, TgError> {
-        if c.quantity.is_empty() {
-            return Err(TgError::ConfError("quantity cannot be empty".to_string()));
-        }
-        let inner = Inner {
-            buy: c.next_buy_price,
-            sell: c.grid_sell_price,
+        let db = Db {
+            buy: c.buy_price,
+            sell: c.sell_price,
             history: Vec::new(),
             profit_ratio: c.profit_ratio,
             double_throw_ratio: c.double_throw_ratio,
-            quantity: c.quantity.to_vec(),
-            step: c.step,
+            quantity: c.quantity,
         };
         Ok(Self {
             symbol,
             market,
             trade,
-            inner,
+            db,
         })
     }
 
     fn is_buy(&self, price: f64) -> bool {
-        self.inner.buy >= price
-    }
-
-    fn buy_quantity(&self) -> f64 {
-        self.get_quantity(true)
+        self.db.buy >= price
     }
 
     fn is_sell(&self, price: f64) -> bool {
-        self.inner.sell < price
+        self.db.sell < price
     }
 
     fn is_air(&self) -> bool {
-        self.inner.step == 0
-    }
-
-    fn sell_quantity(&self) -> f64 {
-        self.get_quantity(false)
+        self.db.history.is_empty()
     }
 
     async fn reset_ratio(&mut self) -> anyhow::Result<()> {
@@ -117,43 +111,37 @@ impl FixedGridService {
             percent_total = v + percent_total;
         }
         let value = percent_total.div(k_lines.len() as f64).mul(100.0);
-        self.inner.double_throw_ratio = value;
-        self.inner.profit_ratio = value;
+        self.db.double_throw_ratio = value;
+        self.db.profit_ratio = value;
         Ok(())
     }
 
-    fn record(&mut self, price: f64) {
-        self.inner.history.push(price);
-    }
-
-    fn modify_price(&mut self, step_add: usize, deal_price: f64, market_price: f64) {
-        self.inner.buy = deal_price.mul(1.0.sub(self.inner.double_throw_ratio.div(100.0)));
-        self.inner.sell = deal_price.mul(1.0.add(self.inner.profit_ratio.div(100.0)));
+    fn modify_price(&mut self, step_add: i8, deal_price: f64, market_price: f64) {
+        self.db.buy = deal_price.mul(1.0.sub(self.db.double_throw_ratio.div(100.0)));
+        self.db.sell = deal_price.mul(1.0.add(self.db.profit_ratio.div(100.0)));
         if self.is_buy(market_price) {
-            self.inner.buy = market_price.mul(1.0.sub(self.inner.double_throw_ratio.div(100.0)));
+            self.db.buy = market_price.mul(1.0.sub(self.db.double_throw_ratio.div(100.0)));
         } else if self.is_sell(market_price) {
-            self.inner.sell = market_price.mul(1.0.add(self.inner.profit_ratio.div(100.0)));
+            self.db.sell = market_price.mul(1.0.add(self.db.profit_ratio.div(100.0)));
         }
-        self.inner.step.add_assign(step_add);
         info!(
             "修改后的补仓价格为:{},修改后的网格价格为:{}.",
-            self.inner.buy, self.inner.sell
+            self.db.buy, self.db.sell
         );
     }
+}
 
-    /// Calculate the number of buy/sell
-    fn get_quantity(&self, buy: bool) -> f64 {
-        let step = if buy {
-            self.inner.step
-        } else {
-            self.inner.step - 1
-        };
-        let quantity = &self.inner.quantity;
-        if step < quantity.len() {
-            quantity[step]
-        } else {
-            *quantity.last().unwrap()
-        }
+impl Db {
+    fn last_record(&self) -> Option<&f64> {
+        self.history.last()
+    }
+
+    fn push_record(&mut self, price: f64) {
+        self.history.push(price);
+    }
+
+    fn pop_record(&mut self) {
+        self.history.pop();
     }
 }
 
@@ -166,5 +154,14 @@ mod tests {
         let mut x: usize = 12;
         // x.add_assign(-1);
         // assert_eq!(x, 11);
+    }
+
+    #[test]
+    fn calc() {
+        // let price = 5;
+        // let price1 = 2;
+        // let price2 = 3;
+        // let x = price.sub(price1).mul(price2);
+        // assert_eq!(x, 9);
     }
 }
